e3b0005965059a8dd5c699040f003c1b75a6f9e9
shadowmoonezios <agushifelde@hotmail.com>
2025-11-08 15:21:42 +0200

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Essens Planer</h1>
+      <p>Plane deine Mahlzeiten und verwalte deine Rezepte!</p>
+    </div>
+  );
+}
+
+export default App;
