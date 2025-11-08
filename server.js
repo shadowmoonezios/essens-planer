a9214685fd31f6fb3bdf222bd831e1e7cd9df615
shadowmoonezios <agushifelde@hotmail.com>
2025-11-08 15:22:57 +0200

    Update MongoDB connection string to use environment variable for database URL

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/essens-planer', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/essens-planer', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
